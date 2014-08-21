a11e1f631f5294ea4bdb5c5dda2ebe83d5a8050d
djzoning <DJZoning@gmail.com>
2014-08-21 13:46:17 +0300

    Just a lil bit change in css and create SignUp.php

---
+++ SignUp.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Bubble Blog</title>
+    <link href="css/style.css" rel="stylesheet" />
+</head>
+<body>
+<div id="container">
+    <img src="./images/cooltext1684135702.png" alt="logo"/>
+    <div id="bg">
+        <form action="#" method="post">
+            <input type="text" name="user" placeholder="Username" />
+            <input type="password" name="pass" placeholder="Password" />
+            <input type="email" name="email" placeholder="E-mail" />
+            <input type="submit" id="blue" value="Sign Up" />
+        </form>
+    </div>
+</div>
+</body>
+</html>
